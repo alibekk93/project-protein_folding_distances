dbc06df453816aa381fc0834a04433b6a72364b7
alibekk93 <34392060+alibekk93@users.noreply.github.com>
2020-03-31 17:15:22 -0400

    Update 01_trHMM_from_BLAST.py
    - Removed extra comments;
    - Removed 'file_path' / 'path' noncense and just left 'path';
    - Updated BLAST file 'path' to include both protein source and expression system species

--- scripts/01_trHMM_from_BLAST.py
+++ scripts/01_trHMM_from_BLAST.py
@@ -5,74 +5,38 @@
 
 # ## 0. Libraries
 
-# In[2]:
-
-
 import pandas as pd
 import sys
 
 
 # ## 1. Import data + filter
 
-# In[ ]:
+path = sys.argv[1]
+# path = 'C:/Users/alibe/Desktop/Project/training_files/Bsubtilis/04_Bsubtilis_EColi_BLAST.csv'
 
-
-# species = sys.argv[1]
-
-species = 'Bsubtilis'
-
-file_path = 'C:/Users/alibe/Desktop/Project/training_files/XXX/04_XXX_BLAST'
-path = file_path + '.csv'
+species = file_path.split('_')[-3]
 BLAST_results = pd.read_csv(path, index_col = False)
 
-
-# In[4]:
-
-
 SS_data = pd.read_csv('C:/Users/alibe/Desktop/SS_data_processed.csv', index_col = False)
-
-
-# In[5]:
-
 
 BLAST_results_filtered = BLAST_results[BLAST_results['%Match'] >= 95]
 BLAST_results_filtered = BLAST_results_filtered[BLAST_results_filtered['MatchLen'] >= 100]
 BLAST_results_filtered = BLAST_results_filtered[BLAST_results_filtered['BitScore'] >= 200]
-
-
-# In[6]:
-
-
-BLAST_results_filtered.reset_index().to_csv(file_path.replace('04', '05') + '_results_filtered' + '.csv')
-
-
-# In[7]:
-
-
 BLAST_results_filtered = BLAST_results_filtered.reset_index()
+
+BLAST_results_filtered.to_csv(path.replace('04', '05').replace('BLAST', 'BLAST_results_filtered'))
 
 
 # ## 2. Create lists
 
-# In[8]:
-
-
 DB_list = []
 multiplier_list = []
-
-
-# In[9]:
-
 
 for i in range(len(BLAST_results_filtered)):
     DB_id = BLAST_results_filtered.DB_Seq.iloc[i]
     multiplier = int((len(DB_id) + 1) / 7)
     multiplier_list.append(multiplier)
     DB_list.append(DB_id.split('_'))
-
-
-# In[10]:
-
 
 Q_list = list(BLAST_results_filtered.Query)
 QS_list = list(BLAST_results_filtered.QueryStart.astype(int))
@@ -83,15 +47,8 @@ DBE_list = list(BLAST_results_filtered.DB_SeqEnd.astype(int))
 
 # ## 3. Create lines for Query
 
-# In[11]:
-
-
 Q_AA = []
 Q_SS = []
-
-
-# In[12]:
-
 
 for i in range(len(Q_list)):
     AA = SS_data.loc[SS_data['Name'] == Q_list[i], 'AA'].iloc[0]
@@ -104,10 +61,6 @@ for i in range(len(Q_list)):
     Q_SS.append(('*' + SS) * int(multiplier_list[i]))
     print('\rQ:{:%}'.format(i / (len(Q_list) - 1)), end = '')
 
-
-# In[13]:
-
-
 Q_AA_line = ''.join(Q_AA)
 Q_8_SS_line = ''.join(Q_SS)
 Q_3_SS_line = Q_8_SS_line.replace('S', 'C').replace('T', 'C').replace('B', 'E').replace('I', 'H').replace('G', 'H')
@@ -115,15 +68,8 @@ Q_3_SS_line = Q_8_SS_line.replace('S', 'C').replace('T', 'C').replace('B', 'E').
 
 # ## 4. Create lines for DB
 
-# In[14]:
-
-
 DB_AA = []
 DB_SS = []
-
-
-# In[15]:
-
 
 for i in range(len(DB_list)):
     for j in DB_list[i]:
@@ -137,33 +83,18 @@ for i in range(len(DB_list)):
         DB_SS.append('*' + SS)
     print('\rDB:{:%}'.format(i / (len(DB_list) - 1)), end = '')
 
-
-# In[16]:
-
-
 DB_AA_line = ''.join(DB_AA)
 DB_8_SS_line = ''.join(DB_SS)
 DB_3_SS_line = DB_8_SS_line.replace('S', 'C').replace('T', 'C').replace('B', 'E').replace('I', 'H').replace('G', 'H')
 
 
-# ## 4. Create .trHMMs from lists
-
-# In[17]:
-
+# ## 5. Create .trHMMs from lists
 
 Q_8_output_path = file_path.replace('BLAST', 'Ecoli').replace('04', '06') + '_8' + '.trHMM'
 DB_8_output_path = file_path.replace('BLAST', species).replace('04', '06') + '_8' + '.trHMM'
 
-
-# In[18]:
-
-
 Q_3_output_path = file_path.replace('BLAST', 'Ecoli').replace('04', '06') + '_3' + '.trHMM'
 DB_3_output_path = file_path.replace('BLAST', species).replace('04', '06') + '_3' + '.trHMM'
-
-
-# In[19]:
-
 
 Q_8_trHMM = open(Q_8_output_path, 'w')
 Q_8_trHMM.write('>AA')
@@ -175,10 +106,6 @@ Q_8_trHMM.write('\n')
 Q_8_trHMM.write(Q_8_SS_line)
 Q_8_trHMM.close()
 
-
-# In[20]:
-
-
 DB_8_trHMM = open(DB_8_output_path, 'w')
 DB_8_trHMM.write('>AA')
 DB_8_trHMM.write('\n')
@@ -188,10 +115,6 @@ DB_8_trHMM.write('>SS')
 DB_8_trHMM.write('\n')
 DB_8_trHMM.write(DB_8_SS_line)
 DB_8_trHMM.close()
-
-
-# In[21]:
-
 
 Q_3_trHMM = open(Q_3_output_path, 'w')
 Q_3_trHMM.write('>AA')
@@ -203,10 +126,6 @@ Q_3_trHMM.write('\n')
 Q_3_trHMM.write(Q_3_SS_line)
 Q_3_trHMM.close()
 
-
-# In[22]:
-
-
 DB_3_trHMM = open(DB_3_output_path, 'w')
 DB_3_trHMM.write('>AA')
 DB_3_trHMM.write('\n')
@@ -216,10 +135,3 @@ DB_3_trHMM.write('>SS')
 DB_3_trHMM.write('\n')
 DB_3_trHMM.write(DB_3_SS_line)
 DB_3_trHMM.close()
-
-
-# In[ ]:
-
-
-
-
